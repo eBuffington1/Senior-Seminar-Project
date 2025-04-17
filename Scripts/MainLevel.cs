9998f368c53ba880433c68e1999a8692bcce96f2
Ethan Buffington <ethan.buffington.1@gmail.com>
2025-04-17 18:52:14 -0500

    Updated music to start when fight starts

--- Scripts/MainLevel.cs
+++ Scripts/MainLevel.cs
@@ -3,17 +3,21 @@ using System;
 
 public partial class MainLevel : Node
 {
-    [Export]
-    AnimationPlayer animPlayer;
+    [Export] AnimationPlayer animPlayer;
+    [Export] AudioStreamPlayer2D musicPlayer;
+
+    [Export] Node preFightNode;
 
     void StartFight(Node2D body)
     {
         animPlayer.Play("Phases/SetupZone");
+        musicPlayer.Play();
+
     }
 
     public void phase01()
     {
-
+        preFightNode.QueueFree();
     }
 
 }
