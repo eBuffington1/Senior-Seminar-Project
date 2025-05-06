f1bed896afa5947f7444cb6fe792b73eb1b93c26
Ethan Buffington <ethan.buffington.1@gmail.com>
2025-05-05 20:14:17 -0500

    Phase 1 Base setup

--- Scripts/main_level.gd
+++ Scripts/main_level.gd
@@ -3,7 +3,7 @@ extends Node2D
 @export_subgroup("Boss Phases")
 var _phase01 = preload("res://Scenes/BossPhases/phase_01.tscn")
 
-func StartFight(body: Node2D) -> void:
+func StartFight(_body: Node2D) -> void:
 	$PreFight/PreFight.play("Phases/SetupZone")
 	$bossTimelinePlayer.play("bossTimelines/bossSeminar")
 	$MusicPlayer.play()
@@ -12,6 +12,7 @@ func CleanPreFight() -> void:
 	$PreFight.queue_free()
 
 func Phase01() -> void:
-	
+	print("test")
 	var instance = _phase01.instantiate()
 	add_child(instance)
+	
